7f2906a2e442c42638751f95b62aee7c6071205d
Arnas Kazlauskas <arnas.kazlauskas@kilo.health>
2021-09-07 19:14:22 +0300

    feat: add cards to physical deck

--- app/Models/PhysicalCard.php
+++ app/Models/PhysicalCard.php
@@ -5,6 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property Deck $deck
+ * @property int $deck_id
+ * @property Card $card
+ * @property int $card_id
+ * @property bool $isDrawn
+ */
 class PhysicalCard extends Model
 {
     public function deck(): BelongsTo
--- database/migrations/2021_09_07_155812_create_physical_cards_table.php
+++ database/migrations/2021_09_07_155812_create_physical_cards_table.php
@@ -16,8 +16,10 @@ class CreatePhysicalCardsTable extends Migration
         Schema::create('physical_cards', function (Blueprint $table) {
             $table->id();
             $table->boolean('isDrawn');
+            $table->unsignedBigInteger('deck_id');
             $table->unsignedBigInteger('card_id');
             $table->timestamps();
+            $table->foreign('deck_id')->references('id')->on('decks');
             $table->foreign('card_id')->references('id')->on('cards');
         });
     }
